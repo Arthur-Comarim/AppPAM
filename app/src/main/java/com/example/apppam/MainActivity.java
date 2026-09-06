92bf05601d8e987beda4570002fff159189e81f0
Arthur Comarim <kiwicato130@gmail.com>
2026-09-05 23:13:12 -0300

    versao2

--- app/src/main/java/com/example/apppam/MainActivity.java
+++ app/src/main/java/com/example/apppam/MainActivity.java
@@ -17,16 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button bntProximo = findViewById(R.id.btnProxima);
+        bntProximo.setOnClickListener ( v -> {
+            Intent intent = new Intent(MainActivity.this, TelaLogin.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-            Button bntProximo = findViewById(R.id.btnProxima);
-            bntProximo.setOnClickListener (v -> {
-                Intent intent = new Intent(MainActivity.this, TelaLogin.class);
-                startActivity(intent);
-            });
+
 
         });
     }
