c6388628d0f2d44daf7ac7ea5a59eb5a79d9d8ff
Arthur Comarim <kiwicato130@gmail.com>
2026-09-07 16:13:05 -0300

    versao3

--- app/src/main/java/com/example/apppam/TelaAcerto.java
+++ app/src/main/java/com/example/apppam/TelaAcerto.java
@@ -1,9 +1,6 @@
 package com.example.apppam;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaLogin extends AppCompatActivity {
+public class TelaAcerto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_login);
-
-        EditText editNome = findViewById(R.id.editNome);
-        String Nome = editNome.getText().toString();
-        EditText editSenha = findViewById(R.id.editSenha);
-        String Senha = editNome.getText().toString();
-
-
-
-
+        setContentView(R.layout.activity_tela_acerto);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
